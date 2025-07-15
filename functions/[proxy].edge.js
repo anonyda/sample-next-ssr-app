4cdca9f15e095beb98010ae096c219a1b4552625
Nida Shaikh <zaynida123@gmail.com>
2025-07-15 15:04:48 +0530

    added utm removal logic

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,10 @@
 export default function handler(request, context) {
    console.log(request.url, 'request');
-   return fetch(request)
+   const url = new URL(request.url);
+   const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+   for (const param of utmParams) {
+    url.searchParams.delete(param);
+   }
+   console.log(`${url}`, 'url after removing utm params');
+   return fetch(`${url}`)
 }
