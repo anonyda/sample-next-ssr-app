e7b0769c2b59ba72f2d8a4dc4ddffe9d1967734f
Nida Shaikh <zaynida123@gmail.com>
2025-07-15 13:44:42 +0530

    added edge funtion

---
+++ functions/[proxy].edge.js
@@ -0,0 +1,11 @@
+export default function handler(request, context) {
+    const parsedUrl = new URL(request.url);
+    console.log(parsedUrl, 'parsedUrl');
+    const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+
+    for (const param of utmParams) {
+        parsedUrl.searchParams.delete(param);
+    }
+    console.log(parsedUrl.toString(), 'parsedUrl after removing utm params');
+    return fetch(parsedUrl.toString())
+}
