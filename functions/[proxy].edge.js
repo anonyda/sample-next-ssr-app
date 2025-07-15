004b96426d2114bfc863335fec510bc7e3f305d3
Nida Shaikh <zaynida123@gmail.com>
2025-07-15 14:03:46 +0530

    added basic proxy function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,11 +1,4 @@
 export default function handler(request, context) {
-    const parsedUrl = new URL(request.url);
-    console.log(parsedUrl, 'parsedUrl');
-    const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
-
-    for (const param of utmParams) {
-        parsedUrl.searchParams.delete(param);
-    }
-    console.log(parsedUrl.toString(), 'parsedUrl after removing utm params');
-    return context.rewrite(parsedUrl.pathname + parsedUrl.search);
+   console.log(request.url, 'request');
+   return fetch(request.url)
 }
